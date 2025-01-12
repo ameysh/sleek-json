8bbb920f27170f1cdc55c238d7530a324cd4585e
Amey Shaligram <40026655+ameysh@users.noreply.github.com>
2025-01-12 21:31:02 +0100

    Add support for JSON5 specification

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import JSON5 from "json5";
+
 function formatJSON() {
   const input = document.getElementById("jsonInput").value;
   const output = document.getElementById("jsonOutput");
@@ -6,9 +8,15 @@ function formatJSON() {
     document.getElementById("tabSpaceSelect").value,
     10
   );
+  const specification = document.getElementById("specificationSelect").value;
 
   try {
-    const json = JSON.parse(input);
+    let json;
+    if (specification === "JSON5") {
+      json = JSON5.parse(input);
+    } else {
+      json = JSON.parse(input);
+    }
     output.value = JSON.stringify(json, null, tabSpaces);
     error.style.display = "none";
   } catch (e) {
